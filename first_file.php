38fc40a473f4ca355dfb66a537f2b997b1be86bf
kristelg <kristelg@greeny.cs.tlu.ee>
2016-09-08 13:15:03 +0300

    added my first lines

---
+++ first_file.php
@@ -0,0 +1,14 @@
+<?php
+
+	$firstName = "Kristel";
+	$lastName = "Mansberg";
+
+// echo võimaldab välja näidata
+	//echo $firstName;
+	//echo $lastName;
+	
+	// Kahe stringi liitmiseks kasutan punkti
+	echo $firstName." ".$lastName;
+
+?>
+
